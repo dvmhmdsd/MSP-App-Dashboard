380898e1c06f4a289c7f3c579013a1ba1eb871ee
mohamedsaad <mahameds19999@gmail.com>
2019-10-17 18:07:18 +0200

    fix

--- front/src/components/projects/Projects.vue
+++ front/src/components/projects/Projects.vue
@@ -54,7 +54,6 @@ export default {
         this.projects = result.data;
         this.loading = false;
         this.msg = "";
-        console.log(result.data)
       })
       .catch(err => {
         this.loading = false;
